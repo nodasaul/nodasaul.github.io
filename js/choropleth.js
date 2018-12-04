d4b0bbec15644243f9ef89e92314a50f40ccef99
nodasaul <nodasaul@gmail.com>
2018-12-04 14:38:36 -0500

    Update choropleth.js

--- js/choropleth.js
+++ js/choropleth.js
@@ -33,7 +33,7 @@ Choropleth.prototype.initVis = function () {
 
     this.color = d3.scale.linear()
         .domain([40, 60, 70, 75, 80])
-        .range(["DarkBlue", "MediumBlue ", "Blue ", "RoyalBlue ", "DodgerBlue "]);
+        .range(["DarkBlue", "MediumBlue ", "Blue ", "RoyalBlue ", "LightSkyBlue"]);
 
     //Define path generator
     this.path = d3.geo.path()
