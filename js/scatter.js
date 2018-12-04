60b4232ae3f1ab87423f4f9e2c9579f0a1229604
nodasaul <nodasaul@gmail.com>
2018-12-04 15:08:17 -0500

    Update scatter.js

--- js/scatter.js
+++ js/scatter.js
@@ -35,7 +35,7 @@ Scatter.prototype.initVis = function () {
 
     this.color = d3.scale.linear()
         .domain([40, 60, 70, 75, 80])
-        .range(["MidnightBlue", "MediumBlue ", "Blue ", "DodgerBlue", "LightSkyBlue"]);
+        .range(["darkred", "red", "orange", "lightgreen", "green"]);
 
     that = this;
     this.brush = d3.svg.brush()
